6f4cfb4d47e0b0784e6cd69c3758f65421b38179
Zero-virus <cristiancastillo.rodriguez@gmail.com>
2018-04-25 17:32:42 -0300

    fix

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package notification2
 
 import (
+	"finciero/notifications_test/sigiriya"
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
-	"github.com/gorilla/mux"
+	paymentsApi "github.com/Finciero/notification_receiver-1/tree/master/cmd/server/khipu"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/urfave/negroni"
 )
 
 type paymentResponse struct {
@@ -15,15 +22,14 @@ type paymentResponse struct {
 	Source        string  `json:"source"`
 }
 
-
-func init() {
+/*func init() {
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, appUsage)
-		for -, c := range subcmds {
+		for _, c := range subcmds {
 			fmt.Fprintf(os.Stderr, "")
 		}
 	}
-}
+}*/
 
 func AllNoti(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
@@ -37,26 +43,53 @@ func CreateNoti(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not")
 }
 
-func main() {
-	flag.Parse()
+var logger *logrus.Logger
 
-	if flag.
+type subcmd struct {
+	name        string
+	description string
+	run         func(args []string)
+}
+
+var subcmds = []subcmd{
+	{"serve", "start web server", serveCmd},
+}
+
+func main() {
+	logger = logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{}
+
+	subcmd := flag.Arg(0)
+	for _, c := range subcmds {
+		if c.name == subcmd {
+			c.run(flag.Args()[1:])
+			return
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "unknow subcmd %q\n", subcmd)
+	fmt.Fprintln(os.Stderr, `Run "thief -h" for usage.`)
+	os.Exit(1)
 }
 
 func serveCmd(args []string) {
+	fs := flag.NewFlagSet("serve", flag.ExitOnError)
+
+	host := fs.String("host", "", "HTTP server host")
+	port := fs.Int("port", 8092, "HTTP server port")
 
 	sigiriyaAPIURL := fs.String("sigiriya-api-url", "http://localhost:8090", "list of allowed origins separated by comma.")
 	sigiriyaToken := fs.String("auth-secret-key", "development_token", "Sigiriya authentication token")
 
 	sigiriyaClient := sigiriya.NewClient(&sigiriya.Config{
 		APIURL: *sigiriyaAPIURL,
-		Token:	*sigiriyaToken,
+		Token:  *sigiriyaToken,
 	})
 	logger.Println("Connected to sigiriya")
 
 	ctxPayments := &paymentsApi.Context{
-		KhipuCLient:	khipuCLient,
-		SigiriyaClient:	sigiriyaClient,
+		KhipuClient:    khipuClient,
+		SigiriyaClient: sigiriyaClient,
 	}
 
 	n := negroni.New(
@@ -64,11 +97,22 @@ func serveCmd(args []string) {
 		negronilogrus.NewMiddlewareFromLogger(logger, "Notifications API"), recovery.JSONRecovery(false),
 	)
 
-
 	mux := http.NewServeMux()
-	mux.Handle("/notifications/khipu/",http.StripPrefix("/notifications/khipu",paymentsApi.Handle(ctxPayments)))
+	mux.Handle("/notifications/khipu/", http.StripPrefix("/notifications/khipu", paymentsApi.Handle(ctxPayments)))
 
 	n.UseHandler(mux)
 
-	addr :=
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	logger.Printf("Start listening on %s", addr)
+	if *port == 443 {
+		logger.Fatal(http.ListenAndServeTLS(addr, *certFile, *keyFile, n))
+	} else {
+		logger.Fatal(http.ListenAndServe(addr, n))
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		logger.fatal(err)
+	}
 }
